fef9c91201f3f736f2cf057fce766c857ad4beaf
truonghaui0910 <truonghaui0910@gmail.com>
2025-07-24 02:50:50 +0700

    ghi lai docker

--- clear_docker.bat
+++ clear_docker.bat
@@ -2,6 +2,7 @@
 
 docker system prune -a --volumes -f
 docker builder prune --all -f 
+tắt docker bằng tay
 wsl --shutdown
-Import-Module Hyper-V & 
+Import-Module Hyper-V
 Optimize-VHD -Path 'D:\WORK\TEAM\DOCKER\DockerDesktopWSL\disk\docker_data.vhdx' -Mode Full
